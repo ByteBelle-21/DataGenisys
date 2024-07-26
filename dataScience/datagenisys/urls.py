fc75044843ac4653a40332074d08b03ccc4769bf
ByteBelle-21 <jrg814@usask.ca>
2024-07-26 19:35:11 -0400

    Created about us page

--- dataScience/datagenisys/urls.py
+++ dataScience/datagenisys/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route='',view=views.home_page,name='home_page')
+    path(route='',view=views.home_page,name='home_page'),
+    path(route='aboutUs/',view=views.about_us,name='about_us_page'),
 ]
--- dataScience/datagenisys/views.py
+++ dataScience/datagenisys/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render
 
 def home_page(request):
     return render(request,'datagenisys/home_page.html')
+
+def about_us(request):
+     return render(request,'datagenisys/about_us.html')
