aa9cf8ebeb50f6bddbe58020a833b4378556ff5b
ByteBelle-21 <jrg814@usask.ca>
2024-07-28 02:11:31 -0400

    Implemented the functionality to perform data analysis

--- dataScience/datagenisys/views.py
+++ dataScience/datagenisys/views.py
@@ -10,9 +10,24 @@ def about_us(request):
      return render(request,'datagenisys/about_us.html')
 
 def get_dataset(request):
+    got_data = False
     if request.method == 'POST':
         csv_file = request.FILES['csvFile']
         target_variable = request.POST['targetVariable']
         dataframe = pd.read_csv(csv_file)
-        return redirect('about_us_page')
-    return HttpResponse("fail")
+        false_target = False
+        if(target_variable  not in dataframe.columns):
+            false_target = True 
+        got_data = True
+        column_info = []
+        for col, dtype in dataframe.dtypes.items():
+            column_info.append((col,dtype,dataframe[col].isnull().sum()))
+    
+
+        context = {
+            'false_target': false_target,
+            'got_data': got_data,
+            'column_info':column_info,
+        }
+        return render(request, 'datagenisys/about_us.html', context)
+    return render(request, 'datagenisys/about_us.html', {'got_data': got_data})
