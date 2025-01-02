0cc968e35f9bbd97ac02b5d67d4437af1b2698c9
ByteBelle-21 <jrg814@usask.ca>
2025-01-02 16:55:20 -0500

    Implementing functionality to focus the project on single dataset

--- dataScience/datagenisys/urls.py
+++ dataScience/datagenisys/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path(route='',view=views.home_page,name='home_page'),
-    path(route='upload/',view=views.get_dataset,name='get_dataset'),
+    
+    path(route='',view=views.get_dataset,name='get_dataset'),
     path('graphs/',view=views.get_graphs, name='get_graphs'),
 ]
--- dataScience/datagenisys/views.py
+++ dataScience/datagenisys/views.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+from django.conf import settings
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .nan_handler import NaN_handler
@@ -8,6 +9,7 @@ from .correlations import get_corr
 from .graphs import graph_generator
 from django.http import JsonResponse
 import json
+import os
 from .machine_learning_models import predictive_model
 
 def home_page(request):
@@ -18,54 +20,52 @@ def about_us(request):
 
 def get_dataset(request):
     got_data = False
-    if request.method == 'POST':
-        if 'csvFile' in request.FILES:
-            csv_file = request.FILES['csvFile']
-            target_variable = request.POST['targetVariable']
-            categorical_columns = request.POST['categoricalNumeric']
-            Numeric_categorical_columns = []
-            if(categorical_columns!="No"):
-                Numeric_categorical_columns = [column.strip() for column in categorical_columns.split(',')]
-            dataframe = pd.read_csv(csv_file)
-            false_target = False
-            if(target_variable  not in dataframe.columns):
-                false_target = True 
-            got_data = True
-            data_initial_info = []
-            data_cleaning_steps = []
-            for col, dtype in dataframe.dtypes.items():
-                data_initial_info.append((col,dtype,dataframe[col].isnull().sum())) 
+    csv_file = os.path.join(settings.BASE_DIR, 'datagenisys', 'static', 'datagenisys', 'alzheimers_disease_data.csv')
+    target_variable = 'Diagnosis'
+    categorical_columns = ''
+    Numeric_categorical_columns = []
+    if(categorical_columns!="No"):
+        Numeric_categorical_columns = [column.strip() for column in categorical_columns.split(',')]
+    dataframe = pd.read_csv(csv_file)
+    false_target = False
+    if(target_variable  not in dataframe.columns):
+        false_target = True 
+    got_data = True
+    data_initial_info = []
+    data_cleaning_steps = []
+    for col, dtype in dataframe.dtypes.items():
+        data_initial_info.append((col,dtype,dataframe[col].isnull().sum())) 
 
-            # Handle the missing values int the dataframe
-            dataframe,data_cleaning_steps = NaN_handler(dataframe,Numeric_categorical_columns, data_cleaning_steps)
-            df_json = dataframe.to_json()
-            datetime_cols = {
-                'original_col' :[],
-                'new_cols' :[]
-            }
+    # Handle the missing values int the dataframe
+    dataframe,data_cleaning_steps = NaN_handler(dataframe,Numeric_categorical_columns, data_cleaning_steps)
+    df_json = dataframe.to_json()
+    datetime_cols = {
+        'original_col' :[],
+        'new_cols' :[]
+    }
 
-            dataframe, data_cleaning_steps,Numeric_categorical_columns,data_encoding_map, datetime_cols = category_encoder(dataframe,data_cleaning_steps,Numeric_categorical_columns,datetime_cols)
-            
-            cleaned_dataset = []
-            for col, dtype in dataframe.dtypes.items():
-                cleaned_dataset.append((col,dtype,dataframe[col].isnull().sum()))  
+    dataframe, data_cleaning_steps,Numeric_categorical_columns,data_encoding_map, datetime_cols = category_encoder(dataframe,data_cleaning_steps,Numeric_categorical_columns,datetime_cols)
+    
+    cleaned_dataset = []
+    for col, dtype in dataframe.dtypes.items():
+        cleaned_dataset.append((col,dtype,dataframe[col].isnull().sum()))  
 
-            correlation_dict = json.dumps(get_corr(dataframe)) 
+    correlation_dict = json.dumps(get_corr(dataframe)) 
 
-            #predictive_model(dataframe,target_variable)
-            context = {
-                'false_target': false_target,
-                'got_data': got_data,
-                'data_initial_info':data_initial_info, 
-                'data_cleaning_steps' : data_cleaning_steps,
-                'cleaned_dataset':cleaned_dataset,
-                'df_json':df_json,
-                'correlation_dict':correlation_dict,
-                'Numeric_categorical_columns':Numeric_categorical_columns,
-                'target_variable':target_variable,
-            }
-            return render(request, 'datagenisys/home_page.html', context)
-    return render(request, 'datagenisys/home_page.html', {'got_data': got_data})
+    #predictive_model(dataframe,target_variable)
+    context = {
+        'false_target': false_target,
+        'got_data': got_data,
+        'data_initial_info':data_initial_info, 
+        'data_cleaning_steps' : data_cleaning_steps,
+        'cleaned_dataset':cleaned_dataset,
+        'df_json':df_json,
+        'correlation_dict':correlation_dict,
+        'Numeric_categorical_columns':Numeric_categorical_columns,
+        'target_variable':target_variable,
+    }
+    return render(request, 'datagenisys/home_page.html', context)
+    
 
 
 def get_graphs(request):
